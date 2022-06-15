cb8ddd292fcb1587102e0fd9800c30a5f18cefe1
Chris Lange <chris_lange_89@hotmail.com>
2022-06-16 09:28:36 +1000

    complete task 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+  if (times < 0) return 'ERROR';
+  if (string === '' || times === 0) return '';
 
+  let res = '';
+  for (let x = 0; x < times; x++) {
+    res += string;
+  }
+  return res;
 };
 
 // Do not edit below this line
