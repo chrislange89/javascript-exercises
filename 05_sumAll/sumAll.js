64df981f72a0e9c94fef087c43c1d4d1b7fda672
Chris Lange <chris_lange_89@hotmail.com>
2022-06-16 10:33:10 +1000

    Complete excersize 05 SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (isNaN(start) || isNaN(end) || typeof(start) === "string" || typeof(end) === "string") return 'ERROR';
+  if (start < 0 || end < 0) return 'ERROR';
 
+  let begin = (start > end) ? end : start;
+  let finish = (start > end) ? start : end;
+
+  let sum = 0;
+  for (let x = begin; x <= finish; x++) {
+    sum += x;
+  }
+  return sum;
 };
 
 // Do not edit below this line
