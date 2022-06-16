8a3c24928408b5c58d1b53f09f01fa44f803efc3
Chris Lange <chris_lange_89@hotmail.com>
2022-06-16 10:48:47 +1000

    Complete excersize 06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let oddYear = (year % 2 !== 0);
+  let divisibleByFour = (year % 4 === 0);
+  let divisibleByHundred = (year % 100 === 0);
+  let divisibleByFourHundred = (year % 400 === 0);
 
+  if (oddYear) return false;
+  if (!divisibleByFour) return false;
+  if (divisibleByHundred) return divisibleByFourHundred; 
+
+  return true;
 };
 
 // Do not edit below this line
