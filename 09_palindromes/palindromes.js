2132a3c11d914b8ea622c9efa571392a97fc2c54
Chris Lange <chris_lange_89@hotmail.com>
2022-11-23 13:42:24 +1000

    complete palindromes exersize

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+  const normalInput = input
+    .toLowerCase()
+    .split('')
+    .filter((letter) => {return (punctuation.indexOf(letter) === -1)})
+    .filter((letter) => {return letter !== ' '})
+    .join('');
+  const reversedInput = normalInput.split('').reverse().join('');
+  console.log(`${normalInput} 
+${reversedInput}`);
+
+  return (reversedInput == normalInput);
 
 };
 
