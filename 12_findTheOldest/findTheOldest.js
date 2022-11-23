d4e89eb09a0aafa4e7cc41f53cdbf244a33944ea
Chris Lange <chris_lange_89@hotmail.com>
2022-11-24 09:04:30 +1000

    complete find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let todaysDate = new Date().getFullYear();
+  let ages = people.map((person) => {
+    let personsAge = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : todaysDate - person.yearOfBirth;
+    let newPerson = {...person, age: personsAge}
+    return newPerson;
+  }).sort((a, b) => {return b.age - a.age});
+  return ages[0];
 };
 
 // Do not edit below this line
