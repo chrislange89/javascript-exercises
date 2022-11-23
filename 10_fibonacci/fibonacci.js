a8421840332d4121e16722a8c2900156d6782f88
Chris Lange <chris_lange_89@hotmail.com>
2022-11-23 14:03:29 +1000

    finish fibonacci exersize

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let series = [1, 1];
+  let inputNum = parseInt(num);
+  if (inputNum < 0) return 'OOPS';
+  if (inputNum == 1) return 1;
+  if (inputNum == 2) return 1;
+  
+  let nextNum;
 
+  for (let i = 2; i < inputNum; i++) {
+    nextNum = series[i - 1] + series [i - 2];
+    series.push(nextNum);
+  }
+
+  return series[series.length - 1];
 };
 
 // Do not edit below this line
